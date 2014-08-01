cbd1952f77fa81c4ba0652fbd3d09f8cd2585af8
Daniel Moran <dmj@tid.es>
2014-08-01 15:38:03 +0200

    ADD Extract url-based CB Actions

--- errors.js
+++ errors.js
@@ -6,8 +6,11 @@ var errors= {
     'USER_NOT_FOUND': {
         exceptionId: 'USER_NOT_FOUND',
         exceptionMessage: 'User credentials not found'
+    },
+    'ACTION_NOT_FOUND': {
+        exceptionId: 'ACTION_NOT_FOUND',
+        exceptionMessage: 'The system wasn\'t able to guess the action type or the request'
     }
-
 };
 
 module.exports = errors;
--- test/unit/extract_csb_actions_test.js
+++ test/unit/extract_csb_actions_test.js
@@ -30,7 +30,50 @@ var serverMocks = require('../tools/serverMocks'),
     should = require('should'),
     request = require('request');
 
+
 describe('Extract Context Broker action from request', function() {
+    var proxy,
+        mockServer,
+        mockApp;
+
+    function testAction(action, options) {
+        return function (done) {
+            var extractionExecuted = false;
+
+            var testExtraction = function (req, res, callback) {
+                should.exist(req.action);
+                req.action.should.equal(action);
+                extractionExecuted = true;
+                callback(null, req, res);
+            };
+
+            proxy.middlewares.push(testExtraction);
+
+            request(options, function (error, response, body) {
+                extractionExecuted.should.equal(true);
+                done();
+            });
+        }
+    }
+
+    beforeEach(function (done) {
+        proxyLib.start(function (error, proxyObj) {
+            proxy = proxyObj;
+
+            serverMocks.start(config.resource.original.port, function (error, server, app) {
+                mockServer = server;
+                mockApp = app;
+                done();
+            });
+        });
+    });
+
+    afterEach(function (done) {
+        proxyLib.stop(proxy, function(error) {
+            serverMocks.stop(mockServer, done);
+        });
+    });
+
     describe('When a create action arrives', function () {
         it('should add the action attribute with value "create" to the request');
     });
@@ -41,18 +84,99 @@ describe('Extract Context Broker action from request', function() {
         it('should add the action attribute with value "delete" to the request');
     });
     describe('When a read action arrives', function () {
-        it('should add the action attribute with value "read" to the request');
+        var options = {
+            uri: 'http://localhost:' + config.resource.proxy.port + '/NGSI10/queryContext',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Fiware-Service': 'frn:contextbroker:551:::',
+                'X-Auth-Token': 'UAidNA9uQJiIVYSCg0IQ8Q'
+            },
+            json: utils.readExampleFile('./test/orionRequests/queryContext.json')
+        };
+
+        beforeEach(function (done) {
+            serverMocks.mockPath('/NGSI10/queryContext', mockApp, done);
+        });
+
+        it('should add the action attribute with value "read" to the request', testAction('read', options));
     });
     describe('When a subscribe action arrives', function () {
-        it('should add the action attribute with value "subscribe" to the request');
+        var options = {
+            uri: 'http://localhost:' + config.resource.proxy.port + '/ngsi10/subscribeContext',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Fiware-Service': 'frn:contextbroker:551:::',
+                'X-Auth-Token': 'UAidNA9uQJiIVYSCg0IQ8Q'
+            },
+            json: utils.readExampleFile('./test/orionRequests/queryContext.json')
+        };
+
+        beforeEach(function (done) {
+            serverMocks.mockPath('/ngsi10/subscribeContext', mockApp, done);
+        });
+
+        it('should add the action attribute with value "subscribe" to the request', testAction('subscribe', options));
     });
     describe('When a register action arrives', function () {
-        it('should add the action attribute with value "register" to the request');
+        var options = {
+            uri: 'http://localhost:' + config.resource.proxy.port + '/ngsi9/registerContext',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Fiware-Service': 'frn:contextbroker:551:::',
+                'X-Auth-Token': 'UAidNA9uQJiIVYSCg0IQ8Q'
+            },
+            json: utils.readExampleFile('./test/orionRequests/queryContext.json')
+        };
+
+        beforeEach(function (done) {
+            serverMocks.mockPath('/ngsi9/registerContext', mockApp, done);
+        });
+
+        it('should add the action attribute with value "register" to the request', testAction('register', options));
     });
     describe('When a discover action arrives', function () {
-        it('should add the action attribute with value "discover" to the request');
+        var options = {
+            uri: 'http://localhost:' + config.resource.proxy.port + '/nsgi9/discoverContextAvailability',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Fiware-Service': 'frn:contextbroker:551:::',
+                'X-Auth-Token': 'UAidNA9uQJiIVYSCg0IQ8Q'
+            },
+            json: utils.readExampleFile('./test/orionRequests/queryContext.json')
+        };
+
+        beforeEach(function (done) {
+            serverMocks.mockPath('/nsgi9/discoverContextAvailability', mockApp, done);
+        });
+
+        it('should add the action attribute with value "discover" to the request', testAction('discover', options));
     });
     describe('When a subscribe-availability action arrives', function () {
-        it('should add the action attribute with value "subscribe-availability" to the request');
+        var options = {
+            uri: 'http://localhost:' + config.resource.proxy.port + '/ngsi9/subscribeContextAvailability',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Fiware-Service': 'frn:contextbroker:551:::',
+                'X-Auth-Token': 'UAidNA9uQJiIVYSCg0IQ8Q'
+            },
+            json: utils.readExampleFile('./test/orionRequests/queryContext.json')
+        };
+
+        beforeEach(function (done) {
+            serverMocks.mockPath('/ngsi9/subscribeContextAvailability', mockApp, done);
+        });
+
+        it('should add the action attribute with value "subscribe-availability" to the request',
+            testAction('subscribe-availability', options));
     });
 });
