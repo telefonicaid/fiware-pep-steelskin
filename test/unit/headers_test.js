dcc63b87d8d3db4b86e102aa69b1174364cb2640
Daniel Moran <dmj@tid.es>
2014-08-05 15:35:42 +0200

    ADD X-Forwarded-For header

---
+++ test/unit/headers_test.js
@@ -0,0 +1,151 @@
+/*
+ * Copyright 2013 Telefonica Investigación y Desarrollo, S.A.U
+ *
+ * This file is part of fiware-orion-pep
+ *
+ * fiware-orion-pep is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * fiware-orion-pep is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with fiware-orion-pep.
+ * If not, seehttp://www.gnu.org/licenses/.
+ *
+ * For those usages not covered by the GNU Affero General Public License
+ * please contact with::[daniel.moranjimenez@telefonica.com]
+ */
+
+'use strict';
+
+var serverMocks = require('../tools/serverMocks'),
+    proxyLib = require('../../lib/fiware-orion-pep'),
+    orionPlugin = require('../../lib/services/OrionPlugin'),
+    config = require('../../config'),
+    utils = require('../tools/utils'),
+    should = require('should'),
+    request = require('request');
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+describe('Control header behavior', function() {
+    var proxy,
+        mockTarget,
+        mockTargetApp,
+        mockAccess,
+        mockAccessApp;
+
+    beforeEach(function (done) {
+        proxyLib.start(function (error, proxyObj) {
+            proxy = proxyObj;
+
+            proxy.middlewares.push(orionPlugin.extractCBAction);
+
+            serverMocks.start(config.resource.original.port, function (error, server, app) {
+                mockTarget = server;
+                mockTargetApp = app;
+                serverMocks.start(config.access.port, function (error, serverAccess, appAccess) {
+                    mockAccess = serverAccess;
+                    mockAccessApp = appAccess;
+                    done();
+                });
+            });
+        });
+    });
+
+    afterEach(function (done) {
+        proxyLib.stop(proxy, function(error) {
+            serverMocks.stop(mockTarget, function () {
+                serverMocks.stop(mockAccess, done);
+            });
+        });
+    });
+
+    describe('When a request to the CB arrives to the proxy without X-Forwarded-For', function () {
+        var options = {
+            uri: 'http://localhost:' + config.resource.proxy.port + '/NGSI10/updateContext',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Fiware-Service': 'frn:contextbroker:551:::',
+                'X-Auth-Token': 'UAidNA9uQJiIVYSCg0IQ8Q'
+            },
+            json: utils.readExampleFile('./test/orionRequests/entityCreation.json')
+        };
+
+        beforeEach(function (done) {
+            serverMocks.mockPath('/validate', mockAccessApp, done);
+            serverMocks.mockPath('/NGSI10/updateContext', mockTargetApp, done);
+        });
+
+        it('should add the X-Forwarded-For header', function (done) {
+            var mockExecuted = false;
+
+            mockAccessApp.handler = function (req, res) {
+                res.set('Content-Type', 'application/xml');
+                res.send(utils.readExampleFile('./test/accessControlResponses/permitResponse.xml', true));
+            };
+
+            mockTargetApp.handler = function (req, res) {
+                mockExecuted = true;
+                should.exist(req.headers['x-forwarded-for']);
+                req.headers['x-forwarded-for'].should.equal('127.0.0.1');
+
+                res.json(200, {});
+            };
+
+            request(options, function (error, response, body) {
+                mockExecuted.should.equal(true);
+                done();
+            });
+        });
+    });
+
+    describe('When a request to the CB arrives to the proxy with the X-Forwarded-For header', function () {
+        var options = {
+            uri: 'http://localhost:' + config.resource.proxy.port + '/NGSI10/updateContext',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Fiware-Service': 'frn:contextbroker:551:::',
+                'X-Auth-Token': 'UAidNA9uQJiIVYSCg0IQ8Q',
+                'X-Forwarded-For': '192.168.2.1'
+            },
+            json: utils.readExampleFile('./test/orionRequests/entityCreation.json')
+        };
+
+        beforeEach(function (done) {
+            serverMocks.mockPath('/validate', mockAccessApp, done);
+            serverMocks.mockPath('/NGSI10/updateContext', mockTargetApp, done);
+        });
+
+        it('should reuse the X-Forwarded-For header', function (done) {
+            var mockExecuted = false;
+
+            mockAccessApp.handler = function (req, res) {
+                res.set('Content-Type', 'application/xml');
+                res.send(utils.readExampleFile('./test/accessControlResponses/permitResponse.xml', true));
+            };
+
+            mockTargetApp.handler = function (req, res) {
+                mockExecuted = true;
+                should.exist(req.headers['x-forwarded-for']);
+                req.headers['x-forwarded-for'].should.equal('192.168.2.1');
+
+                res.json(200, {});
+            };
+
+            request(options, function (error, response, body) {
+                mockExecuted.should.equal(true);
+                done();
+            });
+        });
+    });
+});
