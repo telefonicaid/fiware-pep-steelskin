6f81465fe30e44efa1cfae34ba37f8eb81b95458
Daniel Moran <dmj@tid.es>
2014-08-07 11:07:08 +0200

    ADD Debug logs

--- config.js
+++ config.js
@@ -71,6 +71,7 @@ config.ssl = {
  * Default log level. Can be one of: 'DEBUG', 'INFO', 'WARN', 'ERROR', 'FATAL'
  * @type {string}
  */
-config.logLevel = 'DEBUG';
+config.logLevel = '' +
+    '';
 
 module.exports = config;
