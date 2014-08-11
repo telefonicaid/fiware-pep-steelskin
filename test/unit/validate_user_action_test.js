1b92286fedc48327ca802b01fb7be807abc54919
Daniel Moran <dmj@tid.es>
2014-08-11 09:19:13 +0200

    ADD Tests for Keystone Proxy errors

--- test/unit/validate_user_action_test.js
+++ test/unit/validate_user_action_test.js
@@ -154,4 +154,80 @@ describe('Validate action with Access Control', function() {
             });
         });
     });
+
+    describe('When a request to the CB arrives and the connection to the Keystone Proxy is not working', function() {
+        var options = {
+            uri: 'http://localhost:' + config.resource.proxy.port + '/NGSI10/updateContext',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Fiware-Service': 'frn:contextbroker:551:::',
+                'X-Auth-Token': 'UAidNA9uQJiIVYSCg0IQ8Q'
+            },
+            json: utils.readExampleFile('./test/orionRequests/entityCreation.json')
+        };
+
+        beforeEach(function(done) {
+            serverMocks.stop(mockAccess, function() {
+                serverMocks.mockPath('/NGSI10/updateContext', mockTargetApp, done);
+            });
+        });
+
+        afterEach(function(done) {
+            serverMocks.start(config.access.port, function(error, serverAccess, appAccess) {
+                mockAccess = serverAccess;
+                mockAccessApp = appAccess;
+                done();
+            });
+        });
+
+        it('should reject the request with a 503 error', function(done) {
+            var mockExecuted = false;
+
+            mockAccessApp.handler = function(req, res) {
+                mockExecuted = true;
+                res.json(500, {});
+            };
+
+            request(options, function(error, response, body) {
+                response.statusCode.should.equal(503);
+                done();
+            });
+        });
+    });
+
+
+    describe('When a request to the CB arrives and the Keystone proxy fails to make a proper decision', function() {
+        var options = {
+            uri: 'http://localhost:' + config.resource.proxy.port + '/NGSI10/updateContext',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Fiware-Service': 'frn:contextbroker:551:::',
+                'X-Auth-Token': 'UAidNA9uQJiIVYSCg0IQ8Q'
+            },
+            json: utils.readExampleFile('./test/orionRequests/entityCreation.json')
+        };
+
+        beforeEach(function(done) {
+            serverMocks.mockPath('/validate', mockAccessApp, done);
+            serverMocks.mockPath('/NGSI10/updateContext', mockTargetApp, done);
+        });
+
+        it('should reject the request with a 503 error', function(done) {
+            var mockExecuted = false;
+
+            mockAccessApp.handler = function(req, res) {
+                mockExecuted = true;
+                res.json(500, {});
+            };
+
+            request(options, function(error, response, body) {
+                response.statusCode.should.equal(503);
+                done();
+            });
+        });
+    });
 });
