276ba0dbe6b6cf62f390b113e18cee2ca31b0ac2
Daniel Moran <dmj@tid.es>
2014-12-02 16:21:53 +0100

    ADD User cache

--- test/unit/validate_user_action_test.js
+++ test/unit/validate_user_action_test.js
@@ -360,6 +360,8 @@ describe('Validate action with Access Control', function() {
             currentAuthentication = authenticationMechanisms[1];
 
         beforeEach(function(done) {
+            keystonePlugin.cleanCache();
+
             initializeUseCase(currentAuthentication, function() {
                 async.series([
                     keystonePlugin.invalidate,
@@ -548,6 +550,7 @@ describe('Validate action with Access Control', function() {
             currentAuthentication = authenticationMechanisms[1];
 
         beforeEach(function(done) {
+            keystonePlugin.cleanCache();
             initializeUseCase(currentAuthentication, function() {
                 async.series([
                     async.apply(serverMocks.mockPath, currentAuthentication.path, mockOAuthApp),
