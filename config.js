07e11dcc824f2df6b91bdbb58c7d130da988c52e
Daniel Moran <dmj@tid.es>
2015-02-25 16:30:34 +0100

    FIX Add a default value for the access.disable configuration attribute

--- config.js
+++ config.js
@@ -37,6 +37,11 @@ config.resource = {
  */
 config.access = {
     /**
+     * Indicates whether the access control validation should be enabled. Defaults to false.
+     */
+    disable: false,
+
+    /**
      * Protocol to use to access the Access Control.
      */
     protocol: 'http',
