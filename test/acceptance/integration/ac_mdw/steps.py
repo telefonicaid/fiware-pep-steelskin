79f1bb320a9add7d1599e75c22993ae8a5ed06d7
Jon Calderín Goñi <jon.caldering@gmail.com>
2015-03-09 19:36:26 +0100

    Add headers to the new files

--- test/acceptance/integration/ac_mdw/steps.py
+++ test/acceptance/integration/ac_mdw/steps.py
@@ -1,4 +1,27 @@
-__author__ = 'Jon'
+# -*- coding: utf-8 -*-
+"""
+Copyright 2014 Telefonica Investigación y Desarrollo, S.A.U
+
+This file is part of fiware-orion-pep
+
+fiware-orion-pep is free software: you can redistribute it and/or
+modify it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+
+fiware-orion-pep is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public
+License along with fiware-orion-pep.
+If not, see http://www.gnu.org/licenses/.
+
+For those usages not covered by the GNU Affero General Public License
+please contact with::[iot_support at tid.es]
+"""
+__author__ = 'Jon Calderin Goñi <jon.caldering@gmail.com>'
 from integration.steps_lib.mocks import *
 from integration.steps_lib.proxys import *
 from integration.steps_lib.access_control import *
