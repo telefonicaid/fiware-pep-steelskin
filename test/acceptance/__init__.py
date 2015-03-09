167a6497d79a4a18badd5ea85a87e7eefcd02696
Jon Calderín Goñi <jon.caldering@gmail.com>
2015-03-09 19:44:36 +0100

    Add init file to the root acceptance tests folder

---
+++ test/acceptance/__init__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Copyright 2014 Telefonica Investigación y Desarrollo, S.A.U
+
+This file is part of fiware-orion-pep
+
+fiware-orion-pep is free software: you can redistribute it and/or
+modify it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+
+fiware-orion-pep is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public
+License along with fiware-orion-pep.
+If not, see http://www.gnu.org/licenses/.
+
+For those usages not covered by the GNU Affero General Public License
+please contact with::[iot_support@tid.es]
+"""
+__author__ = 'Jon Calderin Goñi <jon.caldering@gmail.com>'
+
+import os
+
+"""
+Make sure the logs path exists and create it otherwise.
+"""
+if not os.path.exists('logs'):
+    os.makedirs('logs')
