b882a426a4b18855a95281ae3efdfc8dfb6a332c
Fermin Galan Marquez <fermin@tid.es>
2015-05-21 14:11:40 +0200

    FIX replacement strings issue

--- test/tools/utils.js
+++ test/tools/utils.js
@@ -2,16 +2,19 @@
  * Copyright 2014 Telefonica Investigación y Desarrollo, S.A.U
  *
  * This file is part of fiware-pep-steelskin
- *fiware-pep-steelskin-pep is free software: you can redistribute it and/or
+
+ * fiware-pep-steelskin is free software: you can redistribute it and/or
  * modify it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the License,
- * or (at your option) any later versionfiware-pep-steelskinrion-pep is distributed in the hope that it will be useful,
+ * or (at your option) any later version
+ *
+ * fiware-pep-steelskin is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public
- * Licensefiware-pep-steelskinre-orion-pep.
+ * License along with fiware-pep-steelskin.
  * If not, seehttp://www.gnu.org/licenses/.
  *
  * For those usages not covered by the GNU Affero General Public License
