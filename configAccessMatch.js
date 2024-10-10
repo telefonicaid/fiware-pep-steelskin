4a0db45c8f4fc57db3ff9bbbf83da3b9cb75f9b1
Alvaro Vega <alvaro.vegagarcia@telefonica.com>
2024-10-10 10:49:46 +0200

    update CNR and configAccessMatch file

--- configAccessMatch.js
+++ configAccessMatch.js
@@ -7,28 +7,28 @@ var configAccessMatch = {};
 
 // Activity related with a list of users
 configAccessMatch.users = [
-    'user1', 'user2',
+    // 'user1', 'user2',
 ];
 
 // Activity related with request which the following headers
 configAccessMatch.headers = [
-    { "fiware-service": "smartcity" },
-    { "x-real-ip": "127.0.0.1" }
+    // { "fiware-service": "smartcity" },
+    // { "x-real-ip": "127.0.0.1" }
 ];
 
 // Activity related with request including the following subpaths
 configAccessMatch.subpaths = [
-    '/v1',
+    // '/v1',
 ];
 
 // Activity related with request including the following subqueries
 configAccessMatch.subqueries = [
-    'flowControl', 'options',
+    // 'flowControl',
 ];
 
 // Activity related with request including the following strings in body
 configAccessMatch.body = [
-    'legacy'
+    // 'legacy'
 ];
 
 
